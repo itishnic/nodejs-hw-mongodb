cafdbca2061e772d53995d86865de4c1e22334b2
itishnic <122455719+itishnic@users.noreply.github.com>
2024-08-14 20:29:36 +0300

    commit

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -8,15 +8,17 @@ import {
   updateContact,
 } from "../services/contacts.js";
 
-
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contact = await getAllContacts({page,
+    perPage,});
 
   res.json({
     status: 200,
     message: "Successfully found contacts!",
-    data: contacts,
+    data: contact,
   });
 };
 
