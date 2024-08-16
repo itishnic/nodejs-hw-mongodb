f77f9a10c0ab2c34740268c0375b8126264635bd
itishnic <122455719+itishnic@users.noreply.github.com>
2024-08-16 19:37:29 +0300

    commit

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,14 +1,12 @@
-
-
-import { isValidObjectId } from 'mongoose';
-import createHttpError from 'http-errors';
+import { isValidObjectId } from "mongoose";
+import createHttpError from "http-errors";
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    throw createHttpError(400, 'Bad Request');
+  const { contactId } = req.params;
+
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(400, "Bad Request");
   }
 
   next();
 };
-
