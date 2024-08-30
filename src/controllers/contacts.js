2a84b6cd12fff66fca490ae02aa3bee779bf123f
itishnic <122455719+itishnic@users.noreply.github.com>
2024-08-30 23:12:20 +0300

    commit

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -12,6 +12,9 @@ import { parseSortParams } from '../utils/parseSortParams.js';
 
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
+
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
@@ -102,8 +105,16 @@ export const patchContactController = async (req, res, next) => {
 
   let photoUrl;
 
+  // if (photo) {
+  //   photoUrl = await saveFileToUploadDir(photo);
+  // }
+
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
 
   const result = await updateContact(contactId, {
